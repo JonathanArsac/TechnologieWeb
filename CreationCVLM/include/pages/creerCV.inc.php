6dcb915e1db0ccb8e0df29d8c789d727f371af05
Jonathan <jonathan.arsac@etu.unilim.fr>
2018-12-28 19:30:54 +0100

    e
    ef

--- CreationCVLM/include/pages/creerCV.inc.php
+++ CreationCVLM/include/pages/creerCV.inc.php
@@ -2,7 +2,7 @@
 
 <div class="container-fluid" onload="affichageModele()">
   <div class="row">
-    <div class="container-fluid col-sm-12 col-md-4" >
+    <div id="containerCheckBoxCV" class=" text-center container-fluid col-sm-12 col-md-3" >
 
       <div class="custom-control custom-checkbox ">
         <input type="checkbox" class="custom-control-input " id="checkboxEnTete" checked="checked" disabled>
@@ -48,6 +48,10 @@
         <input type="checkbox" class="custom-control-input" id="checkboxReference" onclick="apparaitreZone(this.id)">
         <label class="custom-control-label" for="checkboxReference">Références</label>
       </div>
+
+      <input class="form-control" type="text" name="nomPDF" placeholder="Nom du PDF ">
+      <button> Enregistrer dans mon espace personnel </button>
+      <button> Télécharger </button>
     </div>
     <div class="container-fluid col-sm-12 col-md-8 " id="containerCreationCV" ondrop="drop(event)" ondragover="allowDrop(event)">
 
@@ -180,12 +184,129 @@
 
 <?php
 
-// Instanciation de la classe dérivée
-$pdf = new PDF();
-$pdf->AliasNbPages();
-$pdf->AddPage();
-$pdf->SetFont('Times','',12);
-for($i=1;$i<=40;$i++)
-    $pdf->Cell(0,10,'Impression de la ligne nro '.$i,0,1);
-$pdf->Output('F','filename.pdf');
-?>
+// // Instanciation de la classe dérivée
+// $pdf = new PDF();
+// $pdf->AliasNbPages();
+// $pdf->AddPage();
+// $pdf->SetFont('Times','',12);
+// for($i=1;$i<=40;$i++)
+//     $pdf->Cell(0,10,'Impression de la ligne nro '.$i,0,1);
+// $pdf->Output('F','filename.pdf');
+// ?>
+
+
+
+
+<script>
+
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("form", ev.target.id);
+}
+
+function drop(ev) {
+
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("form");
+    console.log(ev.target.id);
+    if(ev.target.tagName=="FORM"){
+      ev.target.parentNode.insertBefore(document.getElementById(data),ev.target.nextSibling);
+    }else{
+      var parent = ev.target.parentElement;
+      while(parent.tagName!="FORM"){
+        parent = parent.parentElement;
+      }
+        parent.parentNode.insertBefore(document.getElementById(data),parent.nextSibling);
+    }
+
+
+}
+
+function apparaitreZone(nom) {
+    var idcheckBox = nom;
+    var nomZoneCV= "zone"+idcheckBox.substring(8, 50);
+
+    var zoneCV = document.getElementById(nomZoneCV);
+
+    if (document.getElementById(nom).checked == true){
+        zoneCV.style.display = "block";
+    } else {
+       zoneCV.style.display = "none";
+    }
+}
+
+function $_GET(param) {
+  var vars = {};
+  window.location.href.replace( location.hash, '' ).replace(
+    /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+    function( m, key, value ) { // callback
+      vars[key] = value !== undefined ? value : '';
+    }
+  );
+
+  if ( param ) {
+    return vars[param] ? vars[param] : null;
+  }
+  return vars;
+}
+
+window.onload = function(){
+  switch(parseInt($_GET("modele"))){
+    case 0:
+      break;
+    case 1:
+      zoneObjectif.style.display = "block";
+      checkboxObjectif.checked=true;
+      zoneExperience.style.display = "block";
+      checkboxExperience.checked=true;
+      zoneFormation.style.display = "block";
+      checkboxFormation.checked=true;
+      zoneProjet.style.display = "block";
+      checkboxProjet.checked=true;
+      break;
+    case 2:
+
+    zoneProjet.style.display = "block";
+    checkboxProjet.checked=true;
+    zoneRealisation.style.display = "block";
+    checkboxRealisation.checked=true;
+    zonePublication.style.display = "block";
+    checkboxPublication.checked=true;
+    zoneReference.style.display = "block";
+    checkboxReference.checked=true;
+
+      break;
+    case 3:
+    zoneExperience.style.display = "block";
+    checkboxExperience.checked=true;
+    zoneLangue.style.display = "block";
+    checkboxLangue.checked=true;
+    zoneCertification.style.display = "block";
+    checkboxCertification.checked=true;
+
+      break;
+    case 4:
+    zoneObjectif.style.display = "block";
+    checkboxObjectif.checked=true;
+    zoneExperience.style.display = "block";
+    checkboxExperience.checked=true;
+    zoneFormation.style.display = "block";
+    checkboxFormation.checked=true;
+    zoneCompetence.style.display = "block";
+    checkboxCompetence.checked=true;
+    zoneLangue.style.display = "block";
+    checkboxLangue.checked=true;
+    zoneProjet.style.display = "block";
+    checkboxProjet.checked=true;
+
+      break;
+    default:
+
+  }
+}
+</script>
--- autre/technoweb.sql
+++ autre/technoweb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 27 déc. 2018 à 13:50
+-- Généré le :  jeu. 27 déc. 2018 à 17:22
 -- Version du serveur :  5.7.19
--- Version de PHP :  7.0.23
+-- Version de PHP :  7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `technoweb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `candidat`
+--
+
+DROP TABLE IF EXISTS `candidat`;
+CREATE TABLE IF NOT EXISTS `candidat` (
+  `numeroPersonne` int(11) NOT NULL,
+  `numeroOffre` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `candidat`
+--
+
+INSERT INTO `candidat` (`numeroPersonne`, `numeroOffre`) VALUES
+(16, 18),
+(16, 18);
 
 -- --------------------------------------------------------
 
@@ -46,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `offre` (
   `dateDebutOffre` date NOT NULL,
   `dateFinOffre` date NOT NULL,
   PRIMARY KEY (`numeroOffre`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `offre`
@@ -56,7 +76,12 @@ INSERT INTO `offre` (`numeroOffre`, `numeroRecruteur`, `intituleOffre`, `domaine
 (1, 1, 'Equipier(ère)/ Employé(e) de restauration', 'dzd', 'Quand je suis arrivée la première fois chez McDonald’s, on m’avait prévenue que, dans la restauration, je devrai m’accrocher....', 'zd', 'dz', 'dz', 'dz', '2.5', 'France', '', 'fe', '2018-12-26', '2018-12-04'),
 (2, 15, 'Chauffeur Scolaire H/F Personnes Handicapées', 'njilbh', 'Il vous suffit d\'être titulaire du permis B depuis plus de 4 ans, et d\'être disponible le matin et le soir aux heures de ramassage scolaire....', 'vhukvgu', 'kvgukv', 'gukvguk', 'vgukv', 'gukv', 'Limoges (87)', '500 € - 600 € par mois', 'vgkv', '2018-12-12', '2018-12-27'),
 (3, 15, 'Caissier ELS - Etudiant (h/f) ', 'ukv', 'Maintenir un magasin propre et agréable pour les clients. Une activité professionnelle valorisante compatible avec vos horaires de cours....', 'kv', 'gukv', 'gukv', 'uv', 'ukvgk', 'Aixe-sur-Vienne (87)', '', 'kv', '2018-12-12', '2018-12-28'),
-(4, 1, 'Enquêteur Terrain CDI H/F', '', 'LA SOCIÉTÉ : Véritable Start Up, A+ENERGIES a été ouverte en 2010 par deux jeunes entrepreneurs.', '', '', 'Temps partiel', '', '', 'Limoges (87)', '850 €- 2500€ par mois', '', '2018-12-12', '2018-12-12');
+(4, 1, 'Enquêteur Terrain CDI H/F', '', 'LA SOCIÉTÉ : Véritable Start Up, A+ENERGIES a été ouverte en 2010 par deux jeunes entrepreneurs.', '', '', 'Temps partiel', '', '', 'Limoges (87)', '850 €- 2500€ par mois', '', '2018-12-12', '2018-12-12'),
+(17, 17, 'aaaa', 'aaaaaaa', 'aaaaaa', 'aaaaaaaaa', 'aaaaaaa', 'aaaaa', 'aaaaaaa', 'aaaaaaa', 'aaaa', 'aaaaa', 'aaaa', '2018-12-04', '2018-12-13'),
+(18, 17, 'int', 'dom', 'desc', 'mis', 'prof', 'tc', 'to', 'dur', 'lieu', 'four', 'cont', '2000-02-20', '2000-02-20'),
+(19, 17, 'a', '', 'aa', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', '0001-01-01', '0001-01-01'),
+(20, 17, 'a', '', '', '', '', '', '', '', '', '', '', '0001-01-01', '0001-01-01'),
+(21, 17, 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', '0001-01-01', '0001-01-01');
 
 -- --------------------------------------------------------
 
@@ -74,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `personne` (
   `motDePassePersonne` varchar(50) NOT NULL,
   `demandeurPersonne` tinyint(1) NOT NULL,
   PRIMARY KEY (`numeroPersonne`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `personne`
@@ -84,7 +109,8 @@ INSERT INTO `personne` (`numeroPersonne`, `nomPersonne`, `prenomPersonne`, `mail
 (1, 'Arsac', 'Jonathan', 'jonathanarsac@orange.fr', 'root', 'dc76e9f0c0006e8f919e0c515c66dbba3982f785', 0),
 (5, 'Droal', 'Youenn', 'youenn.droal@orange.fr', 'root2', 'dc76e9f0c0006e8f919e0c515c66dbba3982f785', 1),
 (16, 'Pedro', 'Manuel', 'pedromanuel@orange.fr', 'root', '81be5269ea4c1ce4237c5ed1c3868bf2', 1),
-(15, 'Podrich', 'Xavier', 'podrichxavier@orange.fr', 'root', '81be5269ea4c1ce4237c5ed1c3868bf2', 0);
+(15, 'Podrich', 'Xavier', 'podrichxavier@orange.fr', 'root', '81be5269ea4c1ce4237c5ed1c3868bf2', 0),
+(17, 'you', 'you', 'you@mail.Fr', 'you', '5f27888b2201fc64cea2a9b517bc05b1', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
