ba96843c834d7fb38a53c84a6c32c0ea44643d16
Jonathan <jonathan.arsac@etu.unilim.fr>
2018-12-30 19:45:28 +0100

    Update parcourirAllOffres.inc.php

--- CreationCVLM/include/pages/parcourirAllOffres.inc.php
+++ CreationCVLM/include/pages/parcourirAllOffres.inc.php
@@ -5,14 +5,15 @@ $personneManager = new PersonneManager($pdo);
 $offres = $offreManager->getAllOffres();
 ?>
 
-<h4>Toutes les annonces</h4>
+<h4 class="text-center">Toutes les annonces</h4>
 
-<input id="recherche" name="recherche" oninput="rechercher() "type="text" placeholder="Rechercher..."value="" />
-<select class=" col-lg-3 form-control"  id="selector">
-        <option>Lieu</option>
-        <option>Domaine</option>
-</select>
 <div class="col-lg-9 offset-lg-1">
+
+  <input class="form-control  col-lg-3 " id="recherche" name="recherche" oninput="rechercher() "type="text" placeholder="Rechercher..."value="" />
+  <select class=" col-lg-3 form-control"  id="selector">
+          <option>Lieu</option>
+          <option>Domaine</option>
+  </select>
   <div class="table-responsive">
   <table class="table" >
     <thead>
